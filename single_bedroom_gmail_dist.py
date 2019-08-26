34b8eeb841a351a4597a7bb247573e74eabdaff3
Ira Horecka <ira89@icloud.com>
2019-08-25 23:40:24 -0700

    Update gmail distribution function to format single and multi post findings in a single email format.

--- single_bedroom_gmail_dist.py
+++ single_bedroom_gmail_dist.py
@@ -16,56 +16,115 @@ message["From"] = sender_email
 message["To"] = receiver_email
 
 #slight bug in creating updated file, i.e. data_to_email - revise this
+psb.execute_search()
 data = psb.compile_dtfm()
 data_to_email = psb.find_rooms(data)
 
-if len(data_to_email) != 0:
+class PrepEmail:
+    def __init__(self, dtfm):
+        self.dtfm = dtfm
+
+    def single_entry(self):
+        text = """\
+        Hi {name},
+
+        {body}
+        Good luck!
+        - Ira"""
+        html = """\
+        <html>
+        <body>
+            <p>Hi {name},<br><br>
+            {body}<br>
+            Good luck!<br>
+            - Ira
+            </p>
+        </body>
+        </html>
+        """
+        return text, html
+
+    def multiple_entry(self):
+        text = """\
+        Hi {name},
+
+        {body}
+        Good luck!
+        - Ira"""
+        html = """\
+        <html>
+        <body>
+            <p>Hi {name},<br><br>
+            I found some nice places for you:<br>
+            {body}<br>
+            Good luck!<br>
+            - Ira
+            </p>
+        </body>
+        </html>
+        """
+        return text, html
+
+
+class SendEmail:
+    def __init__(self, text, html):
+        self.text = text
+        self.html = html
+
+    def process_shipment(self):
+        # Turn these into plain/html MIMEText objects
+        part1 = MIMEText(self.text, "plain")
+        part2 = MIMEText(self.html, "html")
+
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(part1)
+        message.attach(part2)
+
+        # Create secure connection with server and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(
+                sender_email, receiver_email, message.as_string()
+            )
+
+data_send = PrepEmail(data_to_email)
+text_body = ""
+html_body = ""
+name = 'robert'
+if len(data_to_email) == 1:
     for index,row in data_to_email.iterrows():
-        name = 'robert'
         location = row['Location']
         price = '%.0f' % row['Price']
         url = row['URL']
         title = row['Title']
 
-        # Create the plain-text and HTML version of your message
-        text = """\
-        Hi {name},
-        I found a nice place in {location} for you for ${price} a month.
-        Check this out and let me know if you like it!
-        {url}
-        See you again,
-        Ira"""
-        html = """\
-        <html>
-        <body>
-            <p>Hi {name},<br><br>
-            I found a nice place in {location} for you for ${price} a month.<br>
-            Check posting
-            <a href="{url}">{title}</a> 
-            and let me know if you like it!<br><br>
-            See you again,<br>
-            Ira
-            </p>
-        </body>
-        </html>
-        """
+        text_body += f'I found a nice place in {location.title()} for you for ${price} a month.{url}'
+        html_body += f'I found a nice place in {location.title()} for you for ${price} a month.<br>Posting: <a href="{url}">{title.title()}</a><br>'
 
-    # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text.format(name = name.title(), location = location.title(), price = price, url = url), "plain")
-    part2 = MIMEText(html.format(name = name.title(), location = location.title(), price = price, url = url, title = title), "html")
+    text, html = data_send.single_entry()
+    text = text.format(body = text_body, name = name.title())
+    html = html.format(body = html_body, name = name.title())
+    send_email = SendEmail(text, html)
+    send_email.process_shipment()
+    print('Email sent.')
+elif len(data_to_email) > 1:
+    for index,row in data_to_email.iterrows():
+        location = row['Location']
+        price = '%.0f' % row['Price']
+        url = row['URL']
+        title = row['Title']
 
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
-    message.attach(part1)
-    message.attach(part2)
+        text_body += f'In {location.title()} for ${price} a month. {url}\n'
+        html_body += f'In {location.title()} for ${price} a month.<br>Posting: <a href="{url}">{title.title()}</a><br>'
 
-    # Create secure connection with server and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(
-            sender_email, receiver_email, message.as_string()
-        )
+    text, html = data_send.multiple_entry()
+    text = text.format(body = text_body, name = name.title())
+    html = html.format(body = html_body, name = name.title())
+    send_email = SendEmail(text, html)
+    send_email.process_shipment()
+    print('Email sent.')
 else:
     print('No email sent.')
     pass
