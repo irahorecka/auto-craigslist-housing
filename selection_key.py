126a305e60d2a70e6258b8dff41d27145bcbcd68
Ira Horecka <ira89@icloud.com>
2019-09-07 18:53:13 -0700

    removed deprecated files from repository

--- selection_key.py
+++
@@ -1,8 +0,0 @@
-#2019-08-25: selection keys and filters for craiglist searching
-
-state_keys = ['california']
-selected_reg = ['sfbay']
-selected_cat = ['sub', 'roo']
-district_list = ['oakland','berkeley','richmond','el cerrito','san leandro','alameda','albany','hercules']
-
-name = 'ira'
